e6a39bcf312e1a3d9dc1eb00694a610218a4acb6
Josh Acton <jgacton@protonmail.com>
2022-03-09 12:46:48 +0000

    11 tests passed

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,8 +4,12 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.Optional;
+import java.util.ArrayList;
 
 /**
  * cw-model
@@ -18,7 +22,64 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			Player mrX,
 			ImmutableList<Player> detectives) {
 		// TODO
-		throw new RuntimeException("Implement me!");
+		if(setup.moves.isEmpty()) throw new IllegalArgumentException();
+		if(detectives.equals(null)) throw new NullPointerException();
+		for(int i = 0; i < detectives.size(); i++) {
+			if(detectives.get(i).has(ScotlandYard.Ticket.DOUBLE)) {
+				throw new IllegalArgumentException();
+			}
+		}
+		if()
+		return new GameState() {
+			@Nonnull
+			@Override
+			public GameState advance(Move move) {
+				if(move.equals(null)) throw new IllegalArgumentException();
+				return null;
+			}
+
+			@Nonnull
+			@Override
+			public GameSetup getSetup() {
+				return setup;
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Piece> getPlayers() {
+				return null;
+			}
+
+			@Nonnull
+			@Override
+			public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+				return Optional.empty();
+			}
+
+			@Nonnull
+			@Override
+			public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+				return Optional.empty();
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableList<LogEntry> getMrXTravelLog() {
+				return null;
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Piece> getWinner() {
+				return new ArrayList<Piece>();
+			}
+
+			@Nonnull
+			@Override
+			public ImmutableSet<Move> getAvailableMoves() {
+				return null;
+			}
+		};
 
 	}
 
