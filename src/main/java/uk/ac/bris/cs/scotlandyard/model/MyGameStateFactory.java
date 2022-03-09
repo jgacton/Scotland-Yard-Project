f013b4d49142d2f32710777e219b9e1aba24443d
Jasmeet <jasmeet@Jasmeets-MacBook-Pro.local>
2022-03-09 17:19:55 +0000

    12 tests passed

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -8,8 +8,15 @@ import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
+<<<<<<< HEAD
 import java.util.Optional;
 import java.util.ArrayList;
+=======
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.TreeSet;
+>>>>>>> e2950e0 (12 tests passing)
 
 /**
  * cw-model
@@ -21,21 +28,43 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
+
 		// TODO
 		if(setup.moves.isEmpty()) throw new IllegalArgumentException();
 		if(detectives.equals(null)) throw new NullPointerException();
+<<<<<<< HEAD
+=======
+		if(mrX.equals(null)) throw new NullPointerException();
+		for(int i=0; i<detectives.size(); i++) {
+			for(int j =0; j<detectives.size(); j++) {
+				if(detectives.get(i).piece().equals(detectives.get(j).piece())) {
+					throw new IllegalArgumentException();
+				}
+				if(detectives.get(i).location()==(detectives.get(j).location())) {
+					throw new IllegalArgumentException();
+				}
+			}
+		}
+>>>>>>> e2950e0 (12 tests passing)
 		for(int i = 0; i < detectives.size(); i++) {
 			if(detectives.get(i).has(ScotlandYard.Ticket.DOUBLE)) {
 				throw new IllegalArgumentException();
 			}
 		}
+<<<<<<< HEAD
 		if()
+=======
+>>>>>>> e2950e0 (12 tests passing)
 		return new GameState() {
 			@Nonnull
 			@Override
 			public GameState advance(Move move) {
 				if(move.equals(null)) throw new IllegalArgumentException();
 				return null;
+<<<<<<< HEAD
+=======
+
+>>>>>>> e2950e0 (12 tests passing)
 			}
 
 			@Nonnull
@@ -47,6 +76,10 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			@Nonnull
 			@Override
 			public ImmutableSet<Piece> getPlayers() {
+<<<<<<< HEAD
+=======
+
+>>>>>>> e2950e0 (12 tests passing)
 				return null;
 			}
 
@@ -70,13 +103,29 @@ public final class MyGameStateFactory implements Factory<GameState> {
 
 			@Nonnull
 			@Override
+<<<<<<< HEAD
 			public ImmutableSet<Piece> getWinner() {
 				return new ArrayList<Piece>();
+=======
+			public ImmutableSet<Piece> getWinner(){
+				return null;
+>>>>>>> e2950e0 (12 tests passing)
 			}
 
 			@Nonnull
 			@Override
 			public ImmutableSet<Move> getAvailableMoves() {
+<<<<<<< HEAD
+=======
+				ArrayList<Integer> locations = new ArrayList<Integer>();
+				for(int i =0; i < detectives.size(); i++) {
+					System.out.println(detectives.get(i).location());
+					locations.add(detectives.get(i).location());
+				}
+				for(int i =0; i < locations.size(); i++) {
+
+				}
+>>>>>>> e2950e0 (12 tests passing)
 				return null;
 			}
 		};
