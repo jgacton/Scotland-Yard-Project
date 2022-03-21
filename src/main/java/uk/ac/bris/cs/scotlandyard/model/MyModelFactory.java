5123baa7486f45923c4523e0e9cff886a4cab65a
Josh Acton <jgacton@protonmail.com>
2022-03-21 11:48:54 +0000

    Started implementing ModelFactory, 76 tests passed.

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -4,18 +4,69 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * cw-model
  * Stage 2: Complete this class
  */
 public final class MyModelFactory implements Factory<Model> {
+	private static final class MyModel implements Model {
+		private MyGameStateFactory stateFactory;
+		private Board.GameState state;
+		private Set<Observer> observers = new HashSet<>();
+
+		MyModel(GameSetup setup, Player mrX, ImmutableList<Player> detectives) {
+			this.stateFactory = new MyGameStateFactory();
+			this.state = stateFactory.build(setup, mrX, detectives);
+		}
+
+		@Nonnull
+		@Override
+		public Board getCurrentBoard() {
+			return this.state;
+		}
+
+		@Override
+		public void registerObserver(@Nonnull Observer observer) {
+			this.observers.add(observer);
+		}
+
+		@Override
+		public void unregisterObserver(@Nonnull Observer observer) {
+			this.observers = this.observers.stream().filter(x -> !x.equals(observer)).collect(Collectors.toSet());
+		}
+
+		@Nonnull
+		@Override
+		public ImmutableSet<Observer> getObservers() {
+			return ImmutableSet.copyOf(this.observers);
+		}
+
+		@Override
+		public void chooseMove(@Nonnull Move move) {
+			state.advance(move);
+			if(!state.getWinner().isEmpty()) {
+				for(Observer O : observers) {
+					O.onModelChanged(state, Observer.Event.GAME_OVER);
+				}
+			} else {
+				for(Observer O : observers) {
+					O.onModelChanged(state, Observer.Event.MOVE_MADE);
+				}
+			}
+		}
+
+	}
 
 	@Nonnull @Override public Model build(GameSetup setup,
 	                                      Player mrX,
 	                                      ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Implement me!");
+		return new MyModel(setup, mrX, detectives);
 	}
 }
